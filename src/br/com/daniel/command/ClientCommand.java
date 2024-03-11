a8950d87c962aad5dbacc3bb47fa85adcc6e3e5d
Daniel Silva <danielss.g8@gmail.com>
2024-03-10 21:44:42 -0300

    criação tela inicial terminal
    tela inicial do terminal

--- src/br/com/daniel/command/ClientCommand.java
+++ src/br/com/daniel/command/ClientCommand.java
@@ -1,8 +1,11 @@
 package br.com.daniel.command;
 
 import br.com.daniel.model.Client;
+import br.com.daniel.service.ClientService;
 
 public class ClientCommand {
+	
+	private ClientService clientService;
 	
 	public static Client openTerminal(Client cliente) {
 		
@@ -14,13 +17,16 @@ public class ClientCommand {
 		System.out.println("*                                                                 *");
 		System.out.println("*******************************************************************");
 		System.out.println("*                                                                 *");
-		System.out.println("* Para iniciar, selecione uma opção:                              *");
+		System.out.println("* Para iniciar, digite a opção desejada:                          *");
 		System.out.println("*                                                                 *");
 		System.out.println("* 1- Acessar Conta                                                *");
 		System.out.println("*                                                                 *");
 		System.out.println("* 2- Abrir uma Conta                                              *");
 		System.out.println("*                                                                 *");
 		System.out.println("*******************************************************************");
+		System.out.print(" Terminal: ");
+		
+		ClientService.openTerminal(cliente);
 		
 		return cliente;
 	}
--- src/br/com/daniel/service/ClientService.java
+++ src/br/com/daniel/service/ClientService.java
@@ -1,12 +1,17 @@
 package br.com.daniel.service;
 
+import java.util.Scanner;
+
 import br.com.daniel.model.Client;
 
 public class ClientService {
 	
 	public static Client openTerminal(Client cliente) {
 		
+		String itemSelected;
 		
+		Scanner selected = new Scanner(System.in);
+		itemSelected = selected.nextLine();
 		
 		return cliente;
 	}
